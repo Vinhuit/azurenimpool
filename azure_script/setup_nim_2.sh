8d09adef5087e0908d97abb8348a0c5b78dac459
Vinh Huynh <caubequay00@gmail.com>
2019-01-24 16:53:41 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -9,10 +9,7 @@ unzip beepminer-0.3.4.zip
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
-let donation*=10
 
-for i in `seq 1 4`;
-do
-    cd beepminer-0.3.4
-    timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge
-done
+cd beepminer-0.3.4
+./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge
+
