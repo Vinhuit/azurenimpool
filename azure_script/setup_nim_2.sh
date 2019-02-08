de122718a5ef7230ebea726c55b110ddd4834dc9
Vinh Huynh <caubequay00@gmail.com>
2019-02-08 19:28:33 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -7,7 +7,7 @@ sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-
 wget https://miner.beeppool.org/downloads/beepminer-0.3.4.zip
 unzip beepminer-0.3.4.zip
 
-pool_address1="${pool_address1:-eu.sushipool.com:443}"
+pool_address1="${pool_address1:-eu.nimpool.io:8444}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
 
 cd beepminer-0.3.4
