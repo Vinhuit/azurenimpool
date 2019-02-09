b8d3e4e48cd0b33bb749c87fa4ec28a1c8c78e82
Vinh Huynh <caubequay00@gmail.com>
2019-02-09 11:24:10 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -4,12 +4,12 @@
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-wget https://miner.beeppool.org/downloads/beepminer-0.3.4.zip
-unzip beepminer-0.3.4.zip
+wget https://github.com/skypool-org/skypool-nimiq-miner-release/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip
+unzip skypool-nimiq-v1.3.4-linux-x64.zip
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
 
-cd beepminer-0.3.4
-./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=ivybridge
+cd skypool-nimiq-v1.3.4-linux-x64
+./skypool-node-client
 
