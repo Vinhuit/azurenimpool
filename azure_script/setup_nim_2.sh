f20724124f95bfa6604c2bd9c570a9675181d5b0
Vinh Huynh <caubequay00@gmail.com>
2019-02-09 12:33:04 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -9,9 +9,15 @@ unzip skypool-nimiq-v1.3.4-linux-x64.zip
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
-
-cd skypool-nimiq-v1.3.4-linux-x64
-sed -i 's@NQ48 8CKH BA24 2VR3 N249 N8MN J5XX 74DB 5XJ8@'"$wallet1"'@' config.txt
-sed -i '4 s@*@'"$miner_id"'@' config.txt
-./skypool-node-client
+for i in `seq 1 4`;
+do
+    cd skypool-nimiq-v1.3.4-linux-x64
+    sed -i 's@NQ48 8CKH BA24 2VR3 N249 N8MN J5XX 74DB 5XJ8@'"$wallet1"'@' config.txt
+    sed -i '4 s@*@'"$miner_id"'@' config.txt
+    timeout 1000m ./skypool-node-client   
+done
+#cd skypool-nimiq-v1.3.4-linux-x64
+#sed -i 's@NQ48 8CKH BA24 2VR3 N249 N8MN J5XX 74DB 5XJ8@'"$wallet1"'@' config.txt
+#sed -i '4 s@*@'"$miner_id"'@' config.txt
+#./skypool-node-client
 
