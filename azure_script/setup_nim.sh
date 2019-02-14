c59b16267a94d6f2d0972ed406461c4d0699c0e1
Vinh Huynh <caubequay00@gmail.com>
2019-02-14 14:59:28 +0700

    Update setup_nim.sh

--- azure_script/setup_nim.sh
+++ azure_script/setup_nim.sh
@@ -4,8 +4,8 @@
 apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-#wget https://miner.beeppool.org/downloads/beepminer-0.3.4.zip
-#unzip beepminer-0.3.4.zip
+wget https://miner.beeppool.org/downloads/beepminer-0.3.4.zip
+unzip beepminer-0.3.4.zip
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
@@ -14,12 +14,17 @@ wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_
 wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/lock.mdb -O lock.mdb
 cp -rf ../index-linux ./
 chmod 777 index-linux
+#cd beepminer-0.3.4
+#timeout 5s ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge --miner=100
+timeout 5s ./index-linux --wallet-address="$wallet1" --pool==$pool_address1 --protocol=dumb --statistics=1 --miner=100 --extra-data=$miner_id
+cp -rf data.mdb main-full-consensus
+cp -rf lock.mdb main-full-consensus
+timeout 10m ./index-linux --wallet-address="$wallet1" --pool==$pool_address1 --protocol=dumb --statistics=1 --miner=100 --extra-data=$miner_id
 for i in `seq 1 4`;
 do
-    #cd beepminer-0.3.4
-    #timeout 5s ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge --miner=100
-    timeout 5s ./index-linux --wallet-address="$wallet1" --pool==$pool_address1 --protocol=dumb --statistics=1 --miner=100 --extra-data=$miner_id
-    cp -rf data.mdb main-full-consensus
-    cp -rf lock.mdb main-full-consensus
-    ./index-linux --wallet-address="$wallet1" --pool==$pool_address1 --protocol=dumb --statistics=1 --miner=100 --extra-data=$miner_id
+    cd beepminer-0.3.4
+    timeout 5s ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge --miner=100
+    cp -rf ../main-full-consensus/data.mdb main-light-consensus
+    cp -rf ../main-full-consensus/lock.mdb main-light-consensus
+    ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id --architecture=sandybridge --miner=100
 done
