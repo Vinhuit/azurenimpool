9938c8029542be50671498449bcfa7bf94c1dd9a
Vinh Huynh <caubequay00@gmail.com>
2019-02-21 13:46:34 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -4,7 +4,7 @@
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-wget https://github.com/skypool-org/skypool-nimiq-miner-release/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip
+wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip
 unzip skypool-nimiq-v1.3.4-linux-x64.zip
 
 pool_address1="${pool_address1:-eu.sushipool.com:443}"
@@ -12,6 +12,7 @@ pool_address1="${pool_address1:-eu.sushipool.com:443}"
 for i in `seq 1 4`;
 do
     cd skypool-nimiq-v1.3.4-linux-x64
+    wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.4/libstdc++.so.6
     sed -i 's@NQ48 8CKH BA24 2VR3 N249 N8MN J5XX 74DB 5XJ8@'"$wallet1"'@' config.txt
     sed -i '4 s@*@'"$miner_id"'@' config.txt
     timeout 1000m ./skypool-node-client   
