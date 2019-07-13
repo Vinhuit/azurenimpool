4c40972a9b626cab140614d90099d485edf05599
Vinh Huynh <caubequay00@gmail.com>
2019-07-13 21:32:27 +0700

    Update setup_nim_2.sh

--- azure_script/setup_nim_2.sh
+++ azure_script/setup_nim_2.sh
@@ -4,7 +4,7 @@
 #sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-sudo apt -y install unzip
+apt-get -y install unzip
 wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/skypool-nimiq-v1.3.4-linux-x64.zip
 unzip skypool-nimiq-v1.3.4-linux-x64.zip
 
