dd69a4dc2792065f012ba985c8848c6d25b1b6d6
Vinh Huynh <caubequay00@gmail.com>
2020-02-22 22:38:32 +0700

    Create nimcolar.sh

---
+++ azure_script/nimcolar.sh
@@ -0,0 +1,9 @@
+pkill chisel
+curl https://i.jpillora.com/chisel! | bash 
+chisel client --keepalive 90m  https://$1.herokuapp.com 9050:socks &
+apt install proxychains
+sed -i 's/socks4/socks5/g'  /etc/proxychains.conf
+sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
+sudo proxychains curl ifconfig.me
+wget https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz
+mv nq-miner ma && sudo proxychains ./ma -t cuda -a "NQ56 JVMC 03YP S4DY NU9C 4VER JER8 EJY1 JX9U" -p urp.best:2053
