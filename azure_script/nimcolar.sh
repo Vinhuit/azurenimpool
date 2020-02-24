bdddaec9b2c41bb5b1e3c2ad650c0250d625779f
Vinh Huynh <caubequay00@gmail.com>
2020-02-24 17:23:47 +0700

    Update nimcolar.sh

--- azure_script/nimcolar.sh
+++ azure_script/nimcolar.sh
@@ -1,9 +1,11 @@
 pkill chisel
 curl https://i.jpillora.com/chisel! | bash 
-chisel client --keepalive 90m  https://$1.herokuapp.com 9050:socks &
+chisel client --keepalive 90m  https://$1.herokuapp.com 9050:socks 9051:socks 9052:socks 9053:socks 9054:socks&
 apt install proxychains
-sed -i 's/socks4/socks5/g'  /etc/proxychains.conf
-sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
+wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/pchain.conf -O pchain.conf
+cp -rf pchain.conf /etc/proxychains.conf
+#sed -i 's/socks4/socks5/g'  /etc/proxychains.conf
+#sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
 sudo proxychains curl ifconfig.me
 wget https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz
 name=$RANDOM
