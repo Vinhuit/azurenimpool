fd74c2ae07a746ddb748015cf4cc914a7050db03
Vinh Huynh <caubequay00@gmail.com>
2020-02-25 19:34:31 +0700

    Update nimcolar.sh

--- azure_script/nimcolar.sh
+++ azure_script/nimcolar.sh
@@ -1,13 +1,22 @@
 pkill chisel
 curl https://i.jpillora.com/chisel! | bash 
-./chisel client --keepalive 90m  https://$1.herokuapp.com 9050:socks 9051:socks 9052:socks 9053:socks 9054:socks&
-pgrep -x "chisel" || chisel client --keepalive 90m  https://$1.herokuapp.com 9050:socks 9051:socks 9052:socks 9053:socks 9054:socks&
+port1 =$RANDOM
+port2 =$RANDOM
+port3 =$RANDOM
+port4 =$RANDOM
+port5 =$RANDOM
+./chisel client --keepalive 90m  https://$1.herokuapp.com $port1:socks $port2:socks $port3:socks $port4:socks $port5:socks&
+pgrep -x "chisel" || chisel client --keepalive 90m  https://$1.herokuapp.com $port1:socks $port2:socks $port3:socks $port4:socks $port5:socks&
 apt install proxychains
 wget https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/pchain.conf -O pchain.conf
 cp -rf pchain.conf /etc/proxychains.conf
-#sed -i 's/socks4/socks5/g'  /etc/proxychains.conf
+sed -i 's/9050/$port1/g'  /etc/proxychains.conf
+sed -i 's/9051/$port2/g'  /etc/proxychains.conf
+sed -i 's/9052/$port3/g'  /etc/proxychains.conf
+sed -i 's/9053/$port4/g'  /etc/proxychains.conf
+sed -i 's/9054/$port5/g'  /etc/proxychains.conf
 #sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
 sudo proxychains curl ifconfig.me
-wget https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz >/dev/null
+wget https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz
 name=$RANDOM
 mv nq-miner $name && sudo proxychains ./$name -t cuda -a "NQ56 JVMC 03YP S4DY NU9C 4VER JER8 EJY1 JX9U" -p $2 $3
