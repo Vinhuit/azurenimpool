030e84ce70a3892b1d9bc1b597b163b73ce916a3
Vinh Huynh <caubequay00@gmail.com>
2020-05-27 13:47:37 +0700

    Update nimcolar.sh

--- azure_script/nimcolar.sh
+++ azure_script/nimcolar.sh
@@ -1,21 +1,6 @@
-pkill chisel
-wget -q https://github.com/jpillora/chisel/releases/download/1.3.1/chisel_linux_amd64.gz -O chisel_linux_amd64.gz 
-y | gunzip "chisel_linux_amd64.gz"  && chmod 777 chisel_linux_amd64 
-port1=$RANDOM
-port2=$RANDOM
-port3=$RANDOM
-port4=$RANDOM
-port5=$RANDOM
-./chisel_linux_amd64 client --keepalive 90m $1 $port1:socks $port2:socks $port3:socks $port4:socks $port5:socks& >/dev/null
-apt install -y proxychains
-wget -q https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/pchain.conf -O pchain.conf
-cp -rf pchain.conf /etc/proxychains.conf
-sed -i "s/9050/${port1}/g"  /etc/proxychains.conf
-sed -i "s/9051/${port2}/g"  /etc/proxychains.conf
-sed -i "s/9052/${port3}/g"  /etc/proxychains.conf
-sed -i "s/9053/${port4}/g"  /etc/proxychains.conf
-sed -i "s/9054/${port5}/g"  /etc/proxychains.conf
-sleep 10
+curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/local/bin/retry && chmod +x /usr/local/bin/retry
+wget -q https://raw.githubusercontent.com/Vinhuit/azurenimpool/master/azure_script/chiselport.sh -O chiselport.sh && chmod 777 chiselport.sh
+retry "timeout 2h bash -c './chiselport.sh nimchain--moviescloud.repl.co'"
 #sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
 sudo proxychains curl ifconfig.me
 wget -q https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz 
