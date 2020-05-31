2fb99a8b8f98368169a72f837268fd68ec5c06e8
Vinh Huynh <caubequay00@gmail.com>
2020-05-31 07:19:55 +0700

    Update nimcolar.sh

--- azure_script/nimcolar.sh
+++ azure_script/nimcolar.sh
@@ -21,6 +21,6 @@ sed -i "s/9054/${port5}/g"  /etc/proxychains.conf
 sleep 10
 #sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
 sudo proxychains curl ifconfig.me
-wget -q https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz 
+wget -q https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz -O nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz 
 name=$RANDOM
 mv nq-miner $name && sudo proxychains ./$name -t cuda -a "NQ56 JVMC 03YP S4DY NU9C 4VER JER8 EJY1 JX9U" -p $2 $3 
