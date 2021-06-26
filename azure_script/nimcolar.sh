43063717804b47ab452f6e10441ddd8d157bad70
Vinh Huynh <caubequay00@gmail.com>
2021-06-26 17:07:49 +0700

    Update nimcolar.sh

--- azure_script/nimcolar.sh
+++ azure_script/nimcolar.sh
@@ -21,12 +21,12 @@ sed -i "s/9053/${port4}/g"  /etc/proxychains.conf
 sed -i "s/9054/${port5}/g"  /etc/proxychains.conf
 sleep 10
 #sed -i 's/proxy_dns/#proxy_dns/g'  /etc/proxychains.conf
-sudo proxychains curl ifconfig.me
+proxychains curl ifconfig.me
 name=$RANDOM
 if [ $3 == "nq" ]
 then
     wget -q https://github.com/tomkha/nq-miner/releases/download/0.99.6/nq-miner-linux-0.99.6.tar.gz -O nq-miner-linux-0.99.6.tar.gz && tar xvzf nq-miner-linux-0.99.6.tar.gz 
-    mv nq-miner $name && sudo proxychains ./$name -t cuda -a "NQ56 JVMC 03YP S4DY NU9C 4VER JER8 EJY1 JX9U" -p $2 $3 
+    mv nq-miner $name && proxychains ./$name -t cuda -a "NQ56 JVMC 03YP S4DY NU9C 4VER JER8 EJY1 JX9U" -p $2 $3 
 elif [ $3 == "noncepro" ]
 then
     wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.3.0/noncerpro-cuda-linux-3.3.0.tar.gz -O noncerpro-cuda-linux-3.3.0.tar.gz && tar xvzf noncerpro-cuda-linux-3.3.0.tar.gz
